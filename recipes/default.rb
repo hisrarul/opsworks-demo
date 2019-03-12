0c3a0e5dabfa545411f2d1cb7bc29a542e6b5c5f
hisrarul <35391097+hisrarul@users.noreply.github.com>
2019-03-12 17:08:29 +0530

    updated for redhat

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,12 @@
 #https://learn.chef.io/modules/learn-the-basics/ubuntu/bring-your-own-system/configure-a-package-and-service#/
-apt_update 'Update the apt cache daily' do
-  frequency 86_400
-  action :periodic
-end
+#apt_update 'Update the apt cache daily' do
+#  frequency 86_400
+#  action :periodic
+#end
 
-package 'apache2'
+package 'httpd'
 
-service 'apache2' do
+service 'httpd' do
   supports status: true
   action [:enable, :start]
 end
